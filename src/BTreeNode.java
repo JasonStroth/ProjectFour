67c00e62654a7581b67039b43ea2d10eee206bea
JasonStroth <jason@Jasons-Laptop.attlocal.net>
2017-10-03 19:57:08 -0500

    ProjectFour

---
+++ src/BTreeNode.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jason
+ */
+public class BTreeNode 
+{
+    public String value;        //Current Node value
+    public BTreeNode left;      //left Node
+    public BTreeNode right;     //right Node
+}
